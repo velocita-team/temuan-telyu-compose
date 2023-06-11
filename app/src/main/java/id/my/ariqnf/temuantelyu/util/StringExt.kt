1dc6df4785d4f8895872be7a610dcb97bad78a8e
AriqNaufalF <ariqn55@gmail.com>
2023-06-11 15:27:55 +0700

    Delete duplicate dependencies & rename wrong var name

--- app/src/main/java/id/my/ariqnf/temuantelyu/util/StringExt.kt
+++ app/src/main/java/id/my/ariqnf/temuantelyu/util/StringExt.kt
@@ -1,13 +1,12 @@
 package id.my.ariqnf.temuantelyu.util
 
-import android.util.Patterns
 import androidx.core.util.PatternsCompat
 import java.util.regex.Pattern
 
 private const val MIN_PASS_LENGTH = 6
 private const val PASS_PATTERN = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{4,}$"
 
-private const val MAX_PASS_LENGTH = 70
+private const val MAX_NAME_LENGTH = 70
 private const val FULL_NAME_PATTERN = "^[\\p{L} ]{2,}$"
 
 fun String.isValidEmail(): Boolean {
@@ -26,6 +25,6 @@ fun String.passwordMatches(repeated: String): Boolean {
 
 fun String.isValidName(): Boolean {
     return this.isNotBlank() &&
-            this.length <= MAX_PASS_LENGTH &&
+            this.length <= MAX_NAME_LENGTH &&
             Pattern.compile(FULL_NAME_PATTERN).matcher(this).matches()
 }
